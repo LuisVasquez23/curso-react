a3c7495b30a2b572d991e538bc1000628c6ec1b6
Luis Vasquez <Vluis5457@gmail.com>
2024-11-01 14:34:40 -0600

    Added click event

--- 03-counter-app/src/components/CounterApp.jsx
+++ 03-counter-app/src/components/CounterApp.jsx
@@ -1,10 +1,17 @@
+import { func } from "prop-types";
 import { PropTypes } from "prop-types";
 
 const CounterApp = ({ counter }) => {
+  const handleAdd = () => {};
+
+  const handleSubstract = () => {};
+
   return (
     <>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubstract}>-1</button>
     </>
   );
 };

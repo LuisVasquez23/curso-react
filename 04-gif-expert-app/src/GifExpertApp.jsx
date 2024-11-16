8f7d612dbc5e3b8349ddac6755b5f7280b01b944
luis-pc\luis <vluis5457@gmail.com>
2024-11-16 13:15:37 -0600

    Agregando estilos y estructura a la aplicacion

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const GifExpertApp = () => {
   return (
     <>
+      {/* titutlo */}
       <h1>GifExpertApp</h1>
+      {/* input */}
+      {/* Listado de Gifs */}
+      {/* Gif item */}
     </>
   );
 };

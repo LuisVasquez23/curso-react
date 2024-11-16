dbeb6a84e3253d2a9e09f993c69c5ccc159a6530
luis-pc\luis <vluis5457@gmail.com>
2024-11-16 13:28:34 -0600

    Agregando evento de agregar categoria

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch"]);
 
+  const handleAdd = () => {
+    setCategories([...categories, "HunterXHunter"]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
       {/* input */}
       {/* Listado de Gifs */}
+      <button onClick={handleAdd}>Agregar</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;

444a5be3583d4b83b9cc84920e8916a22bc92a3a
luis-pc\luis <vluis5457@gmail.com>
2024-11-16 13:24:53 -0600

    Creando lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import { useState } from "react";
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
   return (
     <>
-      {/* titutlo */}
       <h1>GifExpertApp</h1>
       {/* input */}
       {/* Listado de Gifs */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
       {/* Gif item */}
     </>
   );

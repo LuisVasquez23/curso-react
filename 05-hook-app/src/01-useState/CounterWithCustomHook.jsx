d6637db718e2f57ff7b5808f80fc58c28839b2cc
Luis Vasquez <Vluis5457@gmail.com>
2024-11-16 21:58:17 -0600

    Adding value for increment and decrement function

--- 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
+++ 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -7,13 +7,13 @@ const CounterWithCustomHook = () => {
     <>
       <h1>Counter with custom hook: {counter}</h1>
       <hr />
-      <button className="btn btn-primary" onClick={increment}>
+      <button className="btn btn-primary" onClick={() => increment()}>
         +1
       </button>
-      <button className="btn btn-primary" onClick={reset}>
+      <button className="btn btn-primary" onClick={() => reset()}>
         Reset
       </button>
-      <button className="btn btn-primary" onClick={decrement}>
+      <button className="btn btn-primary" onClick={() => decrement()}>
         -1
       </button>
     </>

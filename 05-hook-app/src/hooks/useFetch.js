ecf6729b67e5f046de564cdbeb91a387f50367bb
Luis Vasquez <Vluis5457@gmail.com>
2024-11-19 19:55:20 -0600

    useFetch - Custom hook

---
+++ 05-hook-app/src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useFetch = () => {
+  const [state, setState] = useState({
+    data: null,
+    isLoading: true,
+    hasError: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    getFetch();
+  }, []);
+
+  const getFetch = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    hasError: state.hasError,
+  };
+};
+
+export default useFetch;
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import SimpleFormWiithCustomHook from "./01-useEffect/SimpleFormWithCustomHook";
+import MultipleCustomHook from "./03-examples/MultipleCustomHook";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <SimpleFormWiithCustomHook />
-  </StrictMode>
+  // <StrictMode>
+  //   <SimpleFormWiithCustomHook />
+  // </StrictMode>
+  <MultipleCustomHook />
 );

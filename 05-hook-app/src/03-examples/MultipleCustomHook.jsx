67bd679b5b8f74f15792005f53b0bd322903ed46
luis-pc\luis <vluis5457@gmail.com>
2024-11-20 18:39:06 -0600

    Parametrizando custom fetch hook

--- 05-hook-app/src/03-examples/MultipleCustomHook.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHook.jsx
@@ -1,11 +1,17 @@
 import useFetch from "../hooks/useFetch";
 
 const MultipleCustomHook = () => {
-  useFetch();
+  const { data, isLoading, hasError, error } = useFetch(
+    "https://pokeapi.co/api/v2/pokemon/1"
+  );
 
   return (
     <>
-      <h1>MultipleCustomHook</h1>
+      <h3>Información de Pokémon</h3>
+      <hr />
+
+      {isLoading && <p>Cargando...</p>}
+      <pre>{data?.name}</pre>
     </>
   );
 };
--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const useFetch = () => {
+const useFetch = (url) => {
   const [state, setState] = useState({
     data: null,
     isLoading: true,
@@ -9,19 +9,54 @@ const useFetch = () => {
   });
 
   useEffect(() => {
-    getFetch();
-  }, []);
+    getFetch(url);
+  }, [url]);
+
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null,
+    });
+  };
 
   const getFetch = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    setLoadingState();
+
+    const response = await fetch(url);
+
+    // Sleep
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    if (!response.ok) {
+      setState({
+        data: null,
+        isLoading: false,
+        hasError: true,
+        error: {
+          code: response.status,
+          message: response.statusText,
+        },
+      });
+      return;
+    }
+
     const data = await response.json();
-    console.log(data);
+
+    setState({
+      data,
+      isLoading: false,
+      hasError: false,
+      error: null,
+    });
   };
 
   return {
     data: state.data,
     isLoading: state.isLoading,
     hasError: state.hasError,
+    error: state.error,
   };
 };
 

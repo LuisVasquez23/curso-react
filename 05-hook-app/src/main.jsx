9a9a843321f8cee6ef9a9a46607b205dcc454432
luis-pc\luis <vluis5457@gmail.com>
2024-11-20 19:48:35 -0600

    useRef - Primer uso

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import MultipleCustomHook from "./03-examples/MultipleCustomHook";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   //   <SimpleFormWiithCustomHook />
   // </StrictMode>
-  <MultipleCustomHook />
+  <FocusScreen />
 );

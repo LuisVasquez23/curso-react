171e03e155c1a970955dba3a28c8cfeba5a70632
luis-pc\luis <vluis5457@gmail.com>
2024-11-26 18:16:10 -0600

    Using memo from react

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -4,10 +4,11 @@ import "./index.css";
 import MultipleCustomHook from "./03-examples/MultipleCustomHook";
 import { FocusScreen } from "./04-useRef/FocusScreen";
 import Layout from "./05-useLayoutEffect/Layout";
+import Memorize from "./06-memos/Memorize";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   //   <SimpleFormWiithCustomHook />
   // </StrictMode>
-  <Layout />
+  <Memorize />
 );

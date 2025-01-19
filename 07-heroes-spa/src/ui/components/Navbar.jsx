2c4fef2e1ade8b6c4270ec547ce58ad2fe40487d
Luis Vasquez <Vluis5457@gmail.com>
2025-01-18 22:30:58 -0600

    Using usenavigate hook

--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate("/login", {
+      replace: true,
+    });
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark px-5">
       <Link className="navbar-brand" to="/">
@@ -42,7 +50,9 @@ export const Navbar = () => {
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">Luis</span>
 
-          <button className="nav-item nav-link btn">Logout</button>
+          <button className="nav-item nav-link btn" onClick={() => onLogout()}>
+            Logout
+          </button>
         </ul>
       </div>
     </nav>

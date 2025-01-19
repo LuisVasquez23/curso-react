4ab27d351e65c88d79a72f13578e6aa0ab10ee2b
Luis Vasquez <Vluis5457@gmail.com>
2025-01-19 15:57:58 -0600

    use memo on heroeList component

--- 07-heroes-spa/src/heroes/components/HeroeList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroeList.jsx
@@ -1,8 +1,9 @@
 import { HeroeCard } from "./";
 import { getHeroesByPublisher } from "../helpers";
+import { useMemo } from "react";
 
 export const HeroeList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className="row row-cols-1 row-cols-md-2 g-3">

ffd6f5029b82fb0fec3723a2dae7f8d971cc2688
Luis Vasquez <vluis5457@gmail.com>
2025-01-19 19:47:19 -0600

    Added Auth provider

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router";
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
 
   const onLogin = () => {
+    login("Luis Vasquez");
+
     navigate("/", {
       replace: true,
     });
